b60aa0e41d5b987834698b9fdf4970bcd7d722a6
Essie Pearl <e.pearl@vernalis.com>
2025-04-16 16:51:47 +0100

    Add requirements.txt for install of pytorch and related packages

--- setup_env.sh
+++ setup_env.sh
@@ -11,12 +11,8 @@ fi
 
 pip install lmdb
 
-# install PyTorch with pip after creation of conda environment to set specific CUDA version as described here (https://pytorch.org/get-started/locally/)
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
-
-# install PyTorch Geometric and extensions that match the PyTorch and CUDA versions (change versions if required)
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric \
--f https://data.pyg.org/whl/torch-2.6.0+cu124.html
+# install PyTorch and PyG with pip to set specific CUDA version
+pip install -r requirements.txt -f https://data.pyg.org/whl/torch-2.6.0+cu124.html
 
 # optional generation of lockfile:
 # conda install -y conda-lock
