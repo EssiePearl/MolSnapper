c1ff0665fb0108ec922af5d0a61434c5ee155a10
Essie Pearl <e.pearl@vernalis.com>
2025-04-16 16:05:32 +0100

    Add comments to env setup file

--- setup_env.sh
+++ setup_env.sh
@@ -11,8 +11,10 @@ fi
 
 pip install lmdb
 
+# install PyTorch with pip after creation of conda environment to set specific CUDA version as described here (https://pytorch.org/get-started/locally/)
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 
+# install PyTorch Geometric and extensions that match the PyTorch and CUDA versions (change versions if required)
 pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric \
 -f https://data.pyg.org/whl/torch-2.6.0+cu124.html
 
